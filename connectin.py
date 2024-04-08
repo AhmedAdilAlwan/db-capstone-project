302a08ede7ba08bc616d9a5931d75facd1e3c7a6
Ahmed Adil <ahmeda.alwan5@gmail.com>
2024-04-08 11:40:56 +0300

    adding database client

--- connectin.py
+++ connectin.py
@@ -1,1 +1,11 @@
 import mysql.connector as connector
+
+connection = connector.connect(user = "Ahmed", password = "413aBg97J9$L")
+
+cursor = connection.cursor()
+cursor.execute('USE little_lemon')
+show_tables_query = "SHOW tables" 
+cursor.execute(show_tables_query)
+
+data = cursor.fetchall()
+print(data)
