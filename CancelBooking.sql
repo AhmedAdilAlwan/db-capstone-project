2762eaa8eee635358cb93f58d95e8640fdd797e9
Ahmed Adil <ahmeda.alwan5@gmail.com>
2024-04-16 09:13:17 +0300

    a

--- CancelBooking.sql
+++ CancelBooking.sql
@@ -1,4 +1,4 @@
-CREATE DEFINER=`root`@`localhost` PROCEDURE `CancelBooking`(IN BookingID INT)
+CREATE PROCEDURE `CancelBooking`(IN BookingID INT)
 BEGIN
 DELETE FROM booking WHERE booking_id = BookingID;
 END
